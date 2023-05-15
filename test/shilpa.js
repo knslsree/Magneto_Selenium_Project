333d8c5d02357c00a8dbf1f567e5f1d9d35cdbf0
Shilpa <knsl.sree@gmail.com>
2023-05-15 17:45:07 +0200

    Latest version for shirt color

--- test/shilpa.js
+++ test/shilpa.js
@@ -4,27 +4,29 @@ const should = require('chai').should();
 const expect = require('chai').expect;
 
 /*  As a customer,
-I want to be able to search for a product,
+I want to be able to search for a specific product,
 so that I can find the product I want to buy.
 */
 
 //Test grouping:search
 describe('Search for a product', () => {
     //Test case:
-    context('I search for a product',() => {
+    context('I search for a specific product',() => {
         it('I should see the product that i have searched for ', async() => {
             //start the webbrowser
             const driver = await new Builder().forBrowser('firefox').build();
 
             //Search for a product
            try{
+
             //Move to magento site
             await driver.get('https://magento.softwaretestingboard.com/');
+
             //Get the search input
             await driver.wait(until.elementLocated(By.css('#search')),50000);
             await driver.findElement(By.id('search')).sendKeys('shirt for men', Key.RETURN);
 
-            //Find the first product
+            //Find the third product
              await driver.wait(until.elementsLocated(By.css('.item.product.product-item:nth-child(3)'))),10000;
             const product= await driver.findElement(By.css('.item.product.product-item:nth-child(3)'));
 
@@ -33,16 +35,17 @@ describe('Search for a product', () => {
              let productPrice = await product.findElement(By.css('.price'))
 
 
-            // Extra text
+            // Extract the text
             let productTitleText = await productTitle.getText();
             let productPriceText = await productPrice.getText();
 
+            //Verify the text 
             productTitleText.should.equal('Balboa Persistence Tee');
             productPriceText.should.equal('$29.00');
            
 
             //Asserts
-            assert.equal(productTitleText, 'Balboa Persistence Tee');//Builtin Node
+        assert.equal(productTitleText, 'Balboa Persistence Tee');//Builtin Node
         expect(productTitleText).to.equal('Balboa Persistence Tee');// Chai expect
         productTitleText.should.equal('Balboa Persistence Tee');// Chai should
 
